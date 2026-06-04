6a7235bd79900109178ba3ae887768dc3076216e
diasbaskara <diasbaskara@users.noreply.github.com>
2026-06-04 10:45:40 +0700

    Use tailscale-pattern SVG icon loading (Gio.icon_new_for_string)

--- extension.js
+++ extension.js
@@ -43,9 +43,8 @@ class DeepSeekIndicator extends PanelMenu.Button {
 
         const box = new St.BoxLayout({ style_class: 'deepseek-balance-box' });
 
-        this._icon = new St.Label({
-            text: '\uD83D\uDD35',
-            style_class: 'deepseek-icon',
+            gicon: Gio.icon_new_for_string(extensionPath + '/icon-down.svg'),
+            style_class: 'system-status-icon',
         });
         box.add_child(this._icon);
 
